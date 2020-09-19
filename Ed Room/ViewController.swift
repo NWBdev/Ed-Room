b87f15418c23927f0299a88ef34feb293b87e86a
Nathaniel Burciaga <nathanielburciaga@Nathaniels-MBP.home>
2020-09-19 08:20:12 -0700

    first commit

---
+++ Ed Room/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ed Room
+//
+//  Created by Nathaniel Burciaga on 9/19/20.
+//  Copyright © 2020 Appside of the Box. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ed RoomTests/Ed_RoomTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ed_RoomTests.swift
+//  Ed RoomTests
+//
+//  Created by Nathaniel Burciaga on 9/19/20.
+//  Copyright © 2020 Appside of the Box. All rights reserved.
+//
+
+import XCTest
+@testable import Ed_Room
+
+class Ed_RoomTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
